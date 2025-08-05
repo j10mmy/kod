252b7b8328a2abcbd6785f63ed03122981f124f4
j10mmy <alperen.akcelep@gmail.com>
2025-08-05 19:43:44 +0300

    initial commit

--- app/urun/[id]/page.tsx
+++ app/urun/[id]/page.tsx
@@ -1,16 +1,19 @@
-import ProductDetail from './ProductDetail';
+import { type Metadata } from 'next';
 
-export async function generateStaticParams() {
-  return [
-    { id: '1' },
-    { id: '2' },
-    { id: '3' },
-    { id: '4' },
-    { id: '5' },
-    { id: '6' },
-  ];
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+// (Opsiyonel) SEO için metadata fonksiyonu
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  return {
+    title: `Ürün: ${params.id}`,
+  };
 }
 
-export default function ProductPage({ params }: { params: { id: string } }) {
-  return <ProductDetail productId={params.id} />;
+// Sayfa bileşeni
+export default function Page({ params }: Props) {
+  return <div>Ürün ID: {params.id}</div>;
 }
